012f1c2322246c1d99825e6a21b1c45f6c6a7c89
Abhishek Vijayakumar <vijayakumar.abhishek@gmail.com>
2019-09-24 11:46:14 -0400

    Added more abstract methods and smaller classes

--- api.py
+++ api.py
@@ -1,8 +1,39 @@
 # Abstract API class
+from dataclasses import dataclass
+
+
+@dataclass
+class Story:
+    # Dataclass for handling individual stories
+    id: int
+    name: str
+    chapter_count: int
+
+
+class SearchParams:
+    # Class for a set of search parameters
+
+    def __init__(self):
+        # Set search properties
+        pass
 
 
 class API:
 
     def __init__(self):
-        # TODO: Check if way to implement abstract property for host
+        self.host = None
+        raise NotImplementedError("Abstract method for specific APIs")
+
+    # Story processing methods
+
+    def get_story_data(self, story: Story):
+        # Adds story metadata to a story
+        raise NotImplementedError("Abstract method for specific APIs")
+
+    def get_chapter_data(self, story: Story):
+        # Adds chapter metadata to a story
+        raise NotImplementedError("Abstract method for specific APIs")
+
+    def download_chapters(self, story: Story):
+        # Downloads chapters of a story to disk
         raise NotImplementedError("Abstract method for specific APIs")
